84c1c2c8f1bee985532af931f39d4dbbdf93d3c2
Louis Maddox <louismmx@gmail.com>
2020-03-17 21:46:24 +0000

    Add a description class to handle window description parsing

--- src/pyxsys/x_window.py
+++ src/pyxsys/x_window.py
@@ -20,15 +20,6 @@ class Window(list):
         assert line.lstrip().startswith(prefix), bad_input_err_msg
         return
 
-    @staticmethod
-    def id_from_descline(line):
-        """
-        Window ID is either given at the start of a line (when listed as a child)
-        or in the middle of a line, after a colon (when listed as the type of window)
-        """
-        win_id = line.lstrip().split(":")[1].lstrip().split()[0]
-        return win_id
-
     @property
     def win_id(self):
         return self._win_id
@@ -43,23 +34,51 @@ class Window(list):
     def children(self):
         return self._children
 
-
-class RootWindow(Window):
-    def __init__(self, line):
-        self.check_line_entry(line, "Root window")
-        self.win_id = self.id_from_descline(line)
+    @children.setter
+    def children(self, vals):
+        self._children = vals
         return
 
 
-class SourceWindow(Window):
+class DescWindow(Window):
+    def __init__(self, line):
+        super(WindowDesc, self).__init__(self.parse_descline(line))
+        return
+
+    @staticmethod
+    def parse_descline(line):
+        """
+        Window ID is either given at the start of a line (when listed as a child)
+        or in the middle of a line, after a colon (when listed as the type of window)
+        """
+        win_id = line.lstrip().split(":")[1].lstrip().split()[0]
+        if line.endswith("(has no name)") or win_id == "0x0":
+            name = None
+        else:
+            assert line.count('"') > 1, ValueError("Missing enclosing quotation marks")
+            first_quote = line.find('"')
+            assert line.endswith('"'), ValueError("Expected quotation mark at EOL")
+            name = line[first_quote + 1 : -1]
+        return win_id, name
+
+
+class SourceWindow(WindowDesc):
     def __init__(self, line):
         self.check_line_entry(line, "xwininfo: Window")
-        self.win_id = self.id_from_descline(line)
+        # Remove xwininfo output prefix before processing line for ID and window name
+        super(SourceWindow, self).__init__(line[line.find(":") + 1 :])
         return
 
 
-class ParentWindow(Window):
+class RootWindow(WindowDesc):
+    def __init__(self, line):
+        self.check_line_entry(line, "Root window")
+        super(RootWindow, self).__init__(line)
+        return
+
+
+class ParentWindow(WindowDesc):
     def __init__(self, line):
         self.check_line_entry(line, "Parent window")
-        self.win_id = self.id_from_descline(line)
+        super(ParentWindow, self).__init__(line)
         return
